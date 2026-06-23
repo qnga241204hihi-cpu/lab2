4d1d04fc7ddcddf2a2b57bae168a065dc2671f85
qnga241204hihi-cp <qnga2412.hihi@gmail.com>
2026-06-23 11:22:23 +0700

    feat: dev B tinh gpa theo dieu kien

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def tinh_thue_thu_nhap(thu_nhap):
-    # Kết hợp logic: Giảm trừ 4 triệu của A và tính thuế 10% của B
-    thu_nhap_tinh_thue = thu_nhap - 4000000
-    return max(0, thu_nhap_tinh_thue * 0.1)
+def tinh_diem_gpa(diem_so):
+    if diem_so >= 8.5:
+        return 4.0
+    elif diem_so >= 7.0:
+        return 3.0
+    else:
+        return 2.0
+
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
